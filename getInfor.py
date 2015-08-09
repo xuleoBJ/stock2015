704aba4b164ae8866d51bcde2abb5dcd0f640c53
xuleo <leoxu_1982@163.com>
2015-08-09 20:17:57 +0800

    20150809增加了抓取相关网站新闻的爬虫

---
+++ getInfor.py
@@ -0,0 +1,67 @@
+import datetime
+from bs4 import BeautifulSoup
+import urllib2
+import re
+import ConfigParser
+import os
+
+def writeConfig(websites):
+    cf=ConfigParser.ConfigParser()
+    configFilePath="config.ini"
+    #cf.read(configFilePath)
+    if os.path.isfile(configFilePath):
+            os.remove(configFilePath)
+    cf.add_section('webDir')  
+    cf.set('webDir','websites',websites)
+    ##jjcf.set('proxy','port','80')
+    cf.write(open(configFilePath,"w")) 
+'''
+set proxy here
+'''
+enable_proxy = 0  
+proxy_handler = urllib2.ProxyHandler({"http" : 'yourproxy'})  
+null_proxy_handler = urllib2.ProxyHandler({})  
+if enable_proxy:  
+	opener = urllib2.build_opener(proxy_handler)  
+else:  
+	opener = urllib2.build_opener(null_proxy_handler)  
+urllib2.install_opener(opener)  
+url="http://www.sdpc.gov.cn/govszyw/"
+html_page = urllib2.urlopen(url)
+##file_savelink=open("fileSave.txt","w")
+print "starting:"
+##soup = BeautifulSoup(html_page)
+##globalheader=soup.find("header",{'id':"globalheader"})
+##webs=[]
+##for link in globalheader.findAll('a'):
+##    sLink=str(link.get('href'))
+##    if (sLink.find('section')==True):
+##        webs.append(url+sLink)
+##        print sLink
+##writeConfig('|'.join(webs))
+
+storeDir=datetime.date.today().strftime("%Y%m%d")
+
+##if not os.path.exists(storeDir):
+##    os.mkdir(storeDir)
+##    print storeDir+"has created"
+##else:
+##    storeDir+"exists"
+
+
+for urlLink in webs:
+    try:
+        print "start:"+urlLink
+        req=urllib2.Request(urlLink)
+        response=urllib2.urlopen(req)
+        if response.code==200:
+            parsed_html=response.read()
+            print parsed_html.body.find('li').text
+            ##print re.findall('download.*Download',html)
+        print "Job is OK:\n"
+    except urllib2.HTTPError,e:
+        print "server error"
+        print e.code
+    except urllib2.URLError,e:
+        print "URLError:"
+        print e.reason
