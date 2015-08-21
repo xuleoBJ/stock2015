df153e142dcd1044644d662c708351bfbe498f33
xuleoBJ <xuleoBJ@gmail.com>
2015-08-21 16:14:54 +0800

    20150821pm4
    改进模式识别程序

--- stockDateFind.py
+++ stockDateFind.py
@@ -67,18 +67,20 @@ if __name__=="__main__":
     
     
     ## 自动设置查找条件
-    print (u"自动条件查找历史K线：")
+    print (u"根据涨幅，自动条件查找历史K线：")
      ## 是否考虑成交量增加或者减少，1考虑 0 不考虑
     isConsiderVOlume=0 
     
-    kDays=2 ##需要分析的K线天数
+    kDays=3 ##需要分析的K线天数
+    bias=0.5 ##涨幅取值范围，个股用1，大盘指数用0.5
+    if stockID!="999999":
+        bias=1.0
+    
     for i in range(kDays):
         weekDay=convertDateStr2Date(curStock.dateStrList[-kDays+i]).weekday()+1 ##老外的weekDay比我们少一天，周一是0
         print(curStock.dateStrList[-kDays+i],"weekDay_"+str(weekDay),"rate="+str(curStock.riseRateFList[-kDays+i]),"turnOverRate="+str(curStock.riseOfTurnOverFList[-kDays+i]))
-    bias=0.5 ##涨幅取值范围，个股用1，大盘指数用0.5
-    
-    if stockID!="999999":
-        bias=1.0
+
+    ##根据涨幅选择历史K线
     for i in range(-iDaysPeriodUser+kDays,-1):
 	    iCount=0
 	    bSelect=True
@@ -96,6 +98,25 @@ if __name__=="__main__":
                 print(curStock.dateStrList[i],"weekDay_"+str(weekDay),curStock.riseRateFList[i-2],curStock.riseRateFList[i-1],curStock.riseRateFList[i],\
                       "turnOverRate=",curStock.riseOfTurnOverFList[i-2],curStock.riseOfTurnOverFList[i-1],curStock.riseOfTurnOverFList[i-1],\
                       "RiseRateofNextTradeDay: "+str(curStock.riseRateFList[i+1]))
+    
+    ##增加振幅，选择历史K线 
+    print (u"根据震动幅度，自动条件查找历史K线：")
+    for i in range(kDays):
+        weekDay=convertDateStr2Date(curStock.dateStrList[-kDays+i]).weekday()+1 
+        print(curStock.dateStrList[-kDays+i],u"星期",str(weekDay),u"振幅="+str(curStock.waveRateFList[-kDays+i]),u"成交量="+str(curStock.riseOfTurnOverFList[-kDays+i]))
+    for i in range(-iDaysPeriodUser+kDays,-1):
+	    iCount=0
+	    bSelect=True
+	    while iCount<=kDays-1 and bSelect==True:
+		    valueRate=math.floor(curStock.waveRateFList[-iCount-1]/bias)*bias
+		    if not valueRate<=curStock.waveRateFList[i-iCount]<=valueRate+bias:
+			    bSelect=False
+		    iCount=iCount+1
+	    if bSelect==True:
+                weekDay=convertDateStr2Date(curStock.dateStrList[i]).weekday()+1 ##老外的weekDay比我们少一天，周一是0
+                print(curStock.dateStrList[i],"weekDay_"+str(weekDay),curStock.waveRateFList[i-2],curStock.waveRateFList[i-1],curStock.waveRateFList[i],\
+                      "turnOverRate=",curStock.riseOfTurnOverFList[i-2],curStock.riseOfTurnOverFList[i-1],curStock.riseOfTurnOverFList[i-1],\
+                      "RiseRateofNextTradeDay: "+str(curStock.waveRateFList[i+1]))
     for line in lineWrited:
         fileWrited.write(line+'\n')
     fileWrited.close()
