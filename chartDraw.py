4409fc7b2e90ef3953a86453113fbc72e16177cb
xuleoBJ <xuleoBJ@gmail.com>
2015-12-02 16:32:06 +0800

    20151202pm5

--- chartDraw.py
+++ chartDraw.py
@@ -1,37 +1,37 @@
 # -*- coding: UTF-8 -*-
 from __future__ import print_function
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import matplotlib.cbook as cbook
 import matplotlib.ticker as ticker
 import datetime
 from matplotlib.dates import  DateFormatter, WeekdayLocator, HourLocator, \
-     DayLocator, MONDAY
+     DayLocator, MONDAY,YearLocator, MonthLocator
+from Cstock import Stock
 
 def format_date(x, pos=None):
     thisind = np.clip(int(x + 0.5), 0, N - 1)
     return r.date[thisind].strftime('%Y-%m-%d')
 
 if __name__ == "__main__":
-    datafile = u'so_data.csv'
-    r = mlab.csv2rec(datafile, delimiter=';')
-
-# the dates in my example file-set are very sparse (and annoying) change the dates to be sequential
-    for i in range(len(r)-1):
-        r['date'][i+1] = r['date'][i] + datetime.timedelta(days=1)
-
+    shStock=Stock('999999')
+    shStock.list2array()
+    
+    mplDate=mpl.dates.date2num(shStock.dateList)
+    mplData=shStock.dayPriceClosedArray
 # first we'll do it the default way, with gaps on weekends
+    years = YearLocator()   # every year
+    months = MonthLocator()  # every month
+    yearsFmt = DateFormatter('%Y')
     fig, ax = plt.subplots()
-    ax.plot(r['date'], r['close'], 'o-')
+    ax.plot(mplDate, mplData, 'o-')
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(yearsFmt)
+    ax.xaxis.set_minor_locator(months)
+    ax.autoscale_view()
     fig.autofmt_xdate()
 
-# next we'll write a custom formatter
-    N = 20
-    ind = np.arange(N)  # the evenly spaced plot indices
-    fig, ax = plt.subplots()
-    ax.plot(r['date'], r['close'], 'o-')
-    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
-    fig.autofmt_xdate()
 
     plt.show()
--- volumeEnerge.py
+++ volumeEnerge.py
@@ -19,44 +19,47 @@ def mymain():
 
 ##period 是量能情绪控制周期
 def moodIndex(curStock,period):
+    print (u"{}日情绪指数分析".format(period))
 #   计算最近period个交易日内的量能排序,通过量能分析市场情绪 
     sortIndexList=curStock.dayTradeVolumeArray[-period:].argsort()
 #    print sortIndexList ## 输出指数，注意是 [-period:]中的位置
     
-    ##负情绪指数
-    tradeVolmax5=0 
+    ##正情绪指数选取的参数天数
+    numOfmoodDay=5
+    if period<20:
+        numOfmoodDay=3
     print (u"交易量最大的交易日：")
-    for item in sortIndexList[-5:]:
-        tradeVolmax5=tradeVolmax5+curStock.dayTradeVolumeArray[-period:][item]
-        print( curStock.dayStrList[-period:][item] )
-    tradeVolmax5=tradeVolmax5/5
-     
-    ##正情绪指数
-    tradeVolmin5=0 
-    print (u"交易量最小的交易日：")
-    for item in sortIndexList[:5]:
-        tradeVolmin5=tradeVolmax5+curStock.dayTradeVolumeArray[-period:][item]
-        print( curStock.dayStrList[-period:][item] )
-    tradeVolmin5=tradeVolmin5/5
     
-    ##规定tradeVolmin5+(tradeVolmax5-tradeVolmin5)*0.5 为 0.50
+    tradeVolmax=0
+    for item in sortIndexList[-numOfmoodDay:]:
+        tradeVolmax=tradeVolmax+curStock.dayTradeVolumeArray[-period:][item]
+        print( curStock.dayStrList[-period:][item] )
+    tradeVolmax=tradeVolmax/numOfmoodDay
+    
+    tradeVolmin=0
+    print (u"交易量最小的交易日：")
+    for item in sortIndexList[:numOfmoodDay]:
+        tradeVolmin=tradeVolmax+curStock.dayTradeVolumeArray[-period:][item]
+        print( curStock.dayStrList[-period:][item] )
+    tradeVolmin=tradeVolmin/numOfmoodDay
+    
+    ##规定tradeVolmin+(tradeVolmax-tradeVolmin)*0.5 为 0.50
     ##计算情绪指数
 
-    moodIndex50=tradeVolmin5+(tradeVolmax5-tradeVolmin5)*0.5
-    for i in range(5,0,-1):
+    moodIndex50=tradeVolmin+(tradeVolmax-tradeVolmin)*0.5
+    for i in range(100,0,-1):
         moodIndex=(curStock.dayTradeVolumeArray[-i]-moodIndex50)/moodIndex50+0.50
-        print (u"{}情绪指数：{:.2f}".format(curStock.dayStrList[-i],moodIndex))
+        print (u"{}情绪指数：{:.2f},次日涨幅{}".format(curStock.dayStrList[-i],moodIndex,curStock.dayRiseRateFList[-i+1]))
 
 
 if __name__ == "__main__":
     print (u"成交量反应了市场情绪。")
-    shStock=Stock('999999')
+    stockID='999999'
+    shStock=Stock(stockID)
     shStock.list2array()
-    stockID='399001'
-    curStock=Stock(stockID)
-    curStock.list2array()
-    moodIndex(shStock,200)
-    moodIndex(curStock,100)
+    ## 应该考虑长期情绪指数和短期情绪指数
+    moodIndex(shStock,100)
+    moodIndex(shStock,15)
     mymain()
 
 
