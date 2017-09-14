2e3894ae860e2119cab4c17abf39c4257f9afc21
xuleo <leoxu_1982@163.com>
2017-09-14 16:26:54 +0800

    20170914

--- webScrapy/喜马拉雅爬虫/test/parserJson.py
+++
@@ -1,6 +0,0 @@
-import json
-filePathJson = "E:\\stock2015\\webScrapy\\喜马拉雅json\\index_tracks3.json"
-with open(filePathJson, 'r') as f:  
-    data = json.load(f) 
-print(data["html"])
-# print(data["title"])
